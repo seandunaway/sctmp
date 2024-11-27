0c83d152de8516caec34f96cc5fec0463afe735f
seandunaway <git@seandunaway.com>
2024-11-27 12:34:55 -0800

    delay random

--- walk.cpp
+++ walk.cpp
@@ -6,8 +6,11 @@ SCDLLName("walk")
 
 SCSFExport scsf_walk(SCStudyInterfaceRef sc) {
 	int input = -1;
+	int persistent = -1;
+
 	SCInputRef button = sc.Input[++input];
 	SCInputRef delay = sc.Input[++input];
+	SCInputRef delay_random = sc.Input[++input];
 	SCInputRef probability = sc.Input[++input];
 	SCInputRef reward = sc.Input[++input];
 	SCInputRef risk = sc.Input[++input];
@@ -23,7 +26,9 @@ SCSFExport scsf_walk(SCStudyInterfaceRef sc) {
 		button.SetInt(1);
 		button.SetIntLimits(1, MAX_ACS_CONTROL_BAR_BUTTONS);
 		delay.Name = "delay";
-		delay.SetTimeAsSCDateTime(SCDateTime(0, 5, 0, 0));
+		delay.SetTimeAsSCDateTime(SCDateTime(0, 10, 0, 0));
+		delay_random.Name = "delay random";
+		delay_random.SetYesNo(1);
 		probability.Name = "probability";
 		probability.SetInt(50);
 		probability.SetIntLimits(0, 100);
@@ -53,9 +58,21 @@ SCSFExport scsf_walk(SCStudyInterfaceRef sc) {
 	sc.GetTradePosition(position);
 	if (position.PositionQuantity != 0.0 || position.WorkingOrdersExist) return;
 
-	SCDateTime& timestamp = sc.GetPersistentSCDateTimeFast(0);
-	if (timestamp + delay.GetDateTime() > sc.LatestDateTimeForLastBar) return;
-	timestamp = sc.LatestDateTimeForLastBar;
+	SCDateTime& timestamp = sc.GetPersistentSCDateTimeFast(++persistent);
+	if (timestamp >= sc.LatestDateTimeForLastBar) return;
+
+	SCDateTime& lastexit = sc.GetPersistentSCDateTimeFast(++persistent);
+	if (lastexit < position.LastExitDateTime) {
+		lastexit = position.LastExitDateTime;
+
+		int timestamp_delay = delay.GetDateTime().GetTimeInSeconds();
+		if (delay_random.GetYesNo()) timestamp_delay = rand() % timestamp_delay;
+
+		timestamp = lastexit;
+		timestamp.AddSeconds(timestamp_delay);
+
+		return;
+	}
 
 	sc.SendOrdersToTradeService = !sc.GlobalTradeSimulationIsOn;
 
