7da52a40bbf724ba7a8c3465a92d90b065c626c0
seandunaway <git@seandunaway.com>
2024-12-15 11:43:59 -0800

    darkmode border

--- darkmode.cpp
+++ darkmode.cpp
@@ -3,28 +3,49 @@
 
 SCDLLName("darkmode")
 
-SCSFExport scsf_darkmode(SCStudyInterfaceRef sc) {
-	if (sc.SetDefaults) {
+void set_color_mode (HWND hwnd, BOOL darkmode, COLORREF border_color)
+{
+	DwmSetWindowAttribute(hwnd, 19, &darkmode, sizeof(darkmode));
+	DwmSetWindowAttribute(hwnd, 20, &darkmode, sizeof(darkmode));
+	DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &darkmode, sizeof(darkmode));
+	DwmSetWindowAttribute(hwnd, DWMWA_BORDER_COLOR, &border_color, sizeof(border_color));
+}
+
+SCSFExport scsf_darkmode(SCStudyInterfaceRef sc)
+{
+	if (sc.SetDefaults)
+	{
 		sc.DisplayStudyName = 0;
 		sc.GraphName = "darkmode";
 		sc.GraphRegion = 0;
 		return;
 	}
 
-	BOOL darkmode = TRUE;
-	int& initialized = sc.GetPersistentIntFast(0);
-
-	if (sc.LastCallToFunction) {
-		darkmode = FALSE;
-		initialized = 0;
-	}
-
-	if (initialized) return;
+	int persistent = -1;
+	int& initialized = sc.GetPersistentInt(++persistent);
+	int64_t& old_border_color = sc.GetPersistentInt64(++persistent);
 
 	HWND hwnd = sc.GetChartWindowHandle(sc.ChartNumber);
-	DwmSetWindowAttribute(hwnd, 19, &darkmode, sizeof(darkmode));
-	DwmSetWindowAttribute(hwnd, 20, &darkmode, sizeof(darkmode));
 
-	sc.AddMessageToLog(darkmode ? "enabled" : "disabled", 0);
-	initialized = 1;
+	if (!initialized)
+	{
+		DwmGetWindowAttribute(hwnd, DWMWA_BORDER_COLOR, &old_border_color, sizeof(old_border_color));
+
+		set_color_mode(hwnd, true, 0x000000);
+
+		sc.AddMessageToLog("enabled", 0);
+
+		initialized = 1;
+		return;
+	}
+
+	if (sc.LastCallToFunction)
+	{
+		set_color_mode(hwnd, false, (COLORREF) old_border_color);
+
+		sc.AddMessageToLog("disabled", 0);
+
+		initialized = 0;
+		return;
+	}
 }
